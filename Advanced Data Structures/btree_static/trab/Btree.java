b79c3d9aa184036912b0621cd22bba286c9bd75c
harturalcantara <harturalcantara@gmail.com>
2024-04-20 19:38:39 -0300

    adding advanced data structures

---
+++ Advanced Data Structures/btree_static/trab/Btree.java
@@ -0,0 +1,185 @@
+package br.ufc.crateus.eda.btree_static.trab;
+
+public class Btree<Key extends Comparable<Key>, Value>  {
+    
+    private static final int M = 4;
+
+    private Node root;       
+    private int height;      
+    private int n;       
+
+    private static final class Node {
+        private int m;                             // number of children
+        private Entry[] children = new Entry[M];   // the array of children
+
+        // create a node with k children
+        private Node(int k) {
+            m = k;
+        }
+    }
+
+    // internal nodes: only use key and next
+    // external nodes: only use key and value
+    private static class Entry {
+        private Comparable key;
+        private final Object val;
+        private Node next;     // helper field to iterate over array entries
+        public Entry(Comparable key, Object val, Node next) {
+            this.key  = key;
+            this.val  = val;
+            this.next = next;
+        }
+    }
+
+    public Btree() {
+        root = new Node(0);
+    }
+ 
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public int height() {
+        return height;
+    }
+
+    public Value get(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to get() is null");
+        return search(root, key, height);
+    }
+
+    private Value search(Node x, Key key, int ht) {
+        Entry[] children = x.children;
+
+        // external node
+        if (ht == 0) {
+            for (int j = 0; j < x.m; j++) {
+                if (eq(key, children[j].key)) return (Value) children[j].val;
+            }
+        }
+
+        // internal node
+        else {
+            for (int j = 0; j < x.m; j++) {
+                if (j+1 == x.m || less(key, children[j+1].key))
+                    return search(children[j].next, key, ht-1);
+            }
+        }
+        return null;
+    }
+
+    public void put(Key key, Value val) {
+        if (key == null) return;
+        Node u = insert(root, key, val, height); 
+        n++;
+        if (u == null) return;
+
+        // need to split root
+        Node t = new Node(2);
+        t.children[0] = new Entry(root.children[0].key, null, root);
+        t.children[1] = new Entry(u.children[0].key, null, u);
+        root = t;
+        height++;
+    }
+
+    private Node insert(Node h, Key key, Value val, int ht) {
+        int j;
+        Entry t = new Entry(key, val, null);
+
+        // external node
+        if (ht == 0) {
+            for (j = 0; j < h.m; j++) {
+                if (less(key, h.children[j].key)) break;
+            }
+        }
+
+        // internal node
+        else {
+            for (j = 0; j < h.m; j++) {
+                if ((j+1 == h.m) || less(key, h.children[j+1].key)) {
+                    Node u = insert(h.children[j++].next, key, val, ht-1);
+                    if (u == null) return null;
+                    t.key = u.children[0].key;
+                    t.next = u;
+                    break;
+                }
+            }
+        }
+
+        for (int i = h.m; i > j; i--)
+            h.children[i] = h.children[i-1];
+        h.children[j] = t;
+        h.m++;
+        if (h.m < M) return null;
+        else         return split(h);
+    }
+
+    // split node in half
+    private Node split(Node h) {
+        Node t = new Node(M/2);
+        h.m = M/2;
+        for (int j = 0; j < M/2; j++)
+            t.children[j] = h.children[M/2+j]; 
+        return t;    
+    }
+
+    public String toString() {
+        return toString(root, height, "") + "\n";
+    }
+
+    private String toString(Node h, int ht, String indent) {
+        StringBuilder s = new StringBuilder();
+        Entry[] children = h.children;
+
+        if (ht == 0) {
+            for (int j = 0; j < h.m; j++) {
+                s.append(indent + children[j].key + " " + children[j].val + "\n");
+            }
+        }
+        else {
+            for (int j = 0; j < h.m; j++) {
+                if (j > 0) s.append(indent + "(" + children[j].key + ")\n");
+                s.append(toString(children[j].next, ht-1, indent + "     "));
+            }
+        }
+        return s.toString();
+    }
+
+
+    private boolean less(Comparable k1, Comparable k2) {
+        return k1.compareTo(k2) < 0;
+    }
+
+    private boolean eq(Comparable k1, Comparable k2) {
+        return k1.compareTo(k2) == 0;
+    }
+
+    public static void main(String[] args) {
+        Btree<String, String> st = new Btree<String, String>();
+
+        st.put("www.cs.princeton.edu", "128.112.136.12");
+        st.put("www.cs.princeton.edu", "128.112.136.11");
+        st.put("www.princeton.edu",    "128.112.128.15");
+        st.put("www.yale.edu",         "130.132.143.21");
+        st.put("www.simpsons.com",     "209.052.165.60");
+        st.put("www.apple.com",        "17.112.152.32");
+
+        System.out.println("cs.princeton.edu:  " + st.get("www.cs.princeton.edu"));
+        System.out.println("hardvardsucks.com: " + st.get("www.harvardsucks.com"));
+        System.out.println("simpsons.com:      " + st.get("www.simpsons.com"));
+        System.out.println("apple.com:         " + st.get("www.apple.com"));
+        System.out.println("ebay.com:          " + st.get("www.ebay.com"));
+        System.out.println("dell.com:          " + st.get("www.dell.com"));
+        
+        System.out.println();
+        System.out.println("size:    " + st.size());
+        System.out.println("height:  " + st.height());
+
+    }
+
+}
+
