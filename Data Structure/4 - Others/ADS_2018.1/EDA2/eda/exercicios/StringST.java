3c93d457a61c2b191c3020317a57e5eeaacd8df8
harturalcantara <harturalcantara@gmail.com>
2024-06-24 00:46:21 -0300

    TrieST

---
+++ Data Structure/4 - Others/ADS_2018.1/EDA2/eda/exercicios/StringST.java
@@ -0,0 +1,26 @@
+package br.ufc.crateus.eda.exercicios;
+
+import br.ufc.crateus.eda.st.ST;
+
+public interface StringST<V> extends ST<String, V> {
+	
+	void put(String key, V value);
+	
+	V get(String key);
+	
+	void delete(String key);
+	
+	boolean contains(String key);
+	
+	boolean isEmpty();
+	
+	String longestPrefixOf(String str);
+	
+	Iterable<String> keysWithPrefix(String str);
+	
+	Iterable<String> keysThatMatch(String str); // utiliza caractere coringa (.)
+	
+	int size();
+	
+	Iterable<String> keys();
+}
